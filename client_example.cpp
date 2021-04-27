68a310ff909d6e8d626d2c16bd362b4e2815e822
doa379 <doa379@gmail.com>
2021-04-27 23:58:56 +0100

    Init commit

--- client_example.cpp
+++ client_example.cpp
@@ -1,6 +1,24 @@
+#include <iostream>
 #include "socketio.h"
 
-int main()
+
+int main(const int argc, const char *argv[])
 {
+  FILE *fp = fopen("/home/jh/workspace/fxcm/demo_token", "r");
+  char TOKEN[64];
+  fgets(TOKEN, sizeof TOKEN, fp);
+  fclose(fp);
+  const std::string HOST { "api-demo.fxcm.com" },
+    DATA { "{\"pairs\":[\"EUR/USD\",\"AUD/USD\"]}" };
+  std::vector<std::string> H { 
+    "Authorization: Bearer " + std::string(argv[1]) + std::string(TOKEN),
+    "Accept: application/json",
+    "Content-Type: application/json"
+  };
+
+  auto cb { [](const std::string &buffer) { std::cout << buffer; } };
+  SocketIO<HttpsClient> c(1.1, HOST, 443, cb);
+  c.connect();
+  c.run(POST, "/subscribe", H, DATA, 250);
   return 0;
 }
--- socketio.cpp
+++ socketio.cpp
@@ -1,31 +1,4 @@
 #include "socketio.h"
-/*
-HttpsClient c(1.1, HOST, 443);
-std::vector<std::string> H;
-
-int main(const int argc, const char *argv[])
-{
-  FILE *fp = fopen("/home/jh/workspace/fxcm/demo_token", "r");
-  char TOKEN[64];
-  fgets(TOKEN, sizeof TOKEN, fp);
-  fclose(fp);
-  H = std::vector<std::string>({ "Authorization: Bearer " + std::string(argv[1]) + std::string(TOKEN),
-    "Accept: application/json",
-    "Content-Type: application/json" });
-
-  c.set_cb([](const std::string &buffer) { std::cout << buffer; });
-  c.connect();
-  while (1)
-  {
-    c.sendreq(POST, "/subscribe", H, DATA);
-    c.recvreq();
-    std::cout << c.get_response() << std::endl;
-    sleep(1);
-  }
-
-  return 0;
-}
-*/
 
 template<typename T>
 SocketIO<T>::SocketIO(const float httpver, const std::string &host, const unsigned port, const Cb &cb)
@@ -57,6 +30,7 @@ void SocketIO<T>::run(REQUEST req, const std::string &endp, const std::vector<st
     {
       c->sendreq(req, endp, H, DATA);
       c->recvreq();
+      cb(c->get_response());
     }
   });
 }
